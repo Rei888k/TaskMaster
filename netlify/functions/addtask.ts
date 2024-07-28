481fc5cf100fa5175988e2092b8e0696a492e209
Rei888k <kinosh28@gmail.com>
2024-07-28 18:33:36 +0900

    notify用api追加

---
+++ netlify/functions/addtask.ts
@@ -0,0 +1,31 @@
+import { Handler, HandlerResponse } from "@netlify/functions";
+import { logger } from "../../src/logger";
+import { addTask } from "../../src/db";
+import { Task } from "../../src/interface";
+
+export const handler: Handler = async (event, context): Promise<HandlerResponse> => {
+    const task: Task = JSON.parse(event.body!);
+
+    return new Promise((resolve, reject) => {
+        try {
+            addTask(task).then((task) => {
+                resolve({
+                    statusCode: 200,
+                    body: JSON.stringify({ task })
+                })
+            }).catch((error) => {
+                logger.error("addtask", error.message)
+                reject({
+                    statusCode: 500,
+                    body: JSON.stringify({ error: 'Failed to addtask' })
+                })
+            })
+        } catch (error) {
+            logger.error("addtask", error)
+            reject({
+                statusCode: 500,
+                body: JSON.stringify({ error: 'Failed to addtask' })
+            })
+        }
+    })
+};
